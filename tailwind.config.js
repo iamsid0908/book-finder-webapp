0b93fce1ca1c409581032bde42aae1904a9f9465
Siddharth chandrakar <siddharthchandrakar007@gmail.com>
2025-03-16 20:58:03 +0530

    fix:merge conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 // @type {import('tailwindcss').Config}
 import tailwindcssAnimate from "tailwindcss-animate";
-
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
